e3763de2321c98aa3c4a465be8e70ed922c96a35
eugenejac3 <eugene.jackson9@gmail.com>
2022-07-09 10:07:40 -0500

    covered a few component and theme examples before building website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { Button, Typography, styled} from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
+
 
 
 function App() {
+
+  const BlueButton = styled(Button)({
+    backgroundColor:"skyblue",
+        color:"#888",
+        margin:5,
+        "&:hover":{
+          backgroundColor:"lightblue"
+        },
+        "&:disabled":{
+          backgroundColor:"gray",
+          color:"white"
+        }
+  })
+
   return (
     <div>
-     Hello
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+      >
+        SETTINGS
+      </Button>
+      <Button startIcon={<AddIcon />} variant="contained" color="success">
+        ADD NEW POST
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        It uses h1 style put it's a p tag
+      </Typography>
+      <BlueButton>My Button</BlueButton>
+      <BlueButton>Another Button</BlueButton>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {theme} from "./theme"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
