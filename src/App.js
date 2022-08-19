f1ea12882360381943a92e357c2ef1a23880338c
eugenejac3 <eugene.jackson9@gmail.com>
2022-08-19 15:17:11 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Feed from "./components/Feed"
 import Rightbar from "./components/Rightbar"
 import {Box, Stack} from '@mui/material';
 import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { createTheme } from "@mui/system";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
 
 
 
 function App() {
+
+ 
   return (
+    
     <Box>
       <Navbar/>
       <Stack direction="row" spacing={2} justifyContent="space-between">
@@ -15,6 +22,7 @@ function App() {
       <Feed/>
       <Rightbar/>
       </Stack>
+      <Add/>
     </Box>
   );
 }
